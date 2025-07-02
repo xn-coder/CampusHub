8f436f078db7c1fabffbd9d225f6a30ef44ac7ff
Damini Chachane <daminichachane04@gmail.com>
2025-07-02 18:56:03 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- next.config.ts
+++ next.config.ts
@@ -18,6 +18,10 @@ const nextConfig: NextConfig = {
       },
     ],
   },
+  webpack: (config) => {
+    config.resolve.alias.canvas = false;
+    return config;
+  },
 };
 
 export default nextConfig;
