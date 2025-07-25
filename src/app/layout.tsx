debe7371d5b999b9b57051128faa2b21dffae90c
Prashik Likhar <prashiklikhar009@gmail.com>
2025-07-25 06:57:54 +0000

    screenshot restriction for whole application

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -27,7 +27,9 @@ export default function RootLayout({
           enableSystem
           disableTransitionOnChange
         >
-          {children}
+          <div className="no-screenshot">
+            {children}
+          </div>
           <Toaster />
         </ThemeProvider>
       </body>
