c59b2cb2ab4ee90a1f961b109c0b9debb9b0bd76
Prashik Likhar <prashiklikhar009@gmail.com>
2025-07-25 07:27:37 +0000

    done

--- next.config.ts
+++ next.config.ts
@@ -19,6 +19,12 @@ const nextConfig: NextConfig = {
         port: '',
         pathname: '/**',
       },
+      {
+        protocol: 'https',
+        hostname: 'bhlvsndultluvzicvxcl.supabase.co',
+        port: '',
+        pathname: '/**',
+      }
     ],
   },
   webpack: (config) => {
