89361881754ff340406f227bff07a9415aea845b
Prashik Likhar <prashiklikhar009@gmail.com>
2025-08-07 18:39:50 +0000

    remove the files that not used any more

--- src/app/(app)/student/study-material/page.tsx
+++
@@ -1,5 +0,0 @@
-// This page has been removed as per user request.
-// You can safely delete this file from your project.
-export default function StudentStudyMaterialPagePlaceholder() {
-  return null;
-}
